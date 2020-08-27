7460b2472021e247a6d4984686569d43ff337b1d
Philipp <mike.litoris@gmail.com>
2020-08-27 19:49:58 +0200

    Created Repositories

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/repositories/DefaultShoppingRepository.kt
@@ -0,0 +1,61 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingDao
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.PixabayAPI
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+import retrofit2.Response
+import javax.inject.Inject
+
+class DefaultShoppingRepository @Inject constructor(
+    private val shoppingDao: ShoppingDao,
+    private val pixabayAPI: PixabayAPI
+) : ShoppingRepository {
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.insertShoppingItem(shoppingItem)
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.deleteShoppingItem(shoppingItem)
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return shoppingDao.observeAllShoppingItems()
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return shoppingDao.observeTotalPrice()
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return try {
+            val response = pixabayAPI.searchForImage(imageQuery)
+            if(response.isSuccessful) {
+                response.body()?.let {
+                    return@let Resource.success(it)
+                } ?: Resource.error("An unknown error occured", null)
+            } else {
+                Resource.error("An unknown error occured", null)
+            }
+        } catch(e: Exception) {
+            Resource.error("Couldn't reach the server. Check your internet connection", null)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
